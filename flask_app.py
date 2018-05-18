30926701cad2b2f4c466ddc38b96b12de9bee7ce
Ryan Sunga <ryansunga@24dcaea7d349>
2018-05-18 15:24:25 +0000

    Use Flask-Login to track logged in users.

--- flask_app.py
+++ flask_app.py
@@ -17,6 +17,7 @@ from flask import redirect
 from flask import url_for
 from wtforms.validators import ValidationError
 from flask import flash
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
 
 app = Flask(__name__)
 
@@ -28,6 +29,7 @@ nav = Nav(app)
 def create_navbar():
     home_view = View('Home', 'homepage')
     login_view = View('Login', 'login')
+    logout_view = View('Logout', 'logout')
     register_view = View('Register', 'register')
     about_me_view = View('About Me', 'about_me')
     class_schedule_view = View('Class Schedule', 'class_schedule')
@@ -36,11 +38,16 @@ def create_navbar():
                              about_me_view,
                              class_schedule_view,
                              top_ten_songs_view)
-    return Navbar('MySite', home_view, misc_subgroup, login_view, register_view)
+    if current_user.is_authenticated:
+        return Navbar('MySite', home_view, misc_subgroup, logout_view)
+    else:
+        return Navbar('MySite', home_view, misc_subgroup, login_view, register_view)
+
 
 
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -74,7 +81,7 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Sign in')
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15))
     email = db.Column(db.String(150))
@@ -85,6 +92,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    @login.user_loader
+    def load_user(user_id):
+        return User.query.filter_by(id=int(user_id)).first()
+
 
 @app.route('/')
 def homepage():
@@ -113,6 +124,7 @@ def register():
         new_user.set_password(form.password.data)
         db.session.add(new_user)
         db.session.commit()
+        login_user(new_user)
         return redirect(url_for('homepage'))
     return render_template('register.html', form=form)
 
@@ -124,17 +136,22 @@ def top_ten_songs():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('homepage'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if not user or not user.check_password(form.password.data):
             flash('Username or password is incorrect.', 'danger')
             return render_template('login.html', form=form)
-        return 'Welcome ' + user.username + '!'
+        login_user(user)
+        return redirect(url_for('homepage'))
     return render_template('login.html', form=form)
 
-
-
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
 
 if __name__ == '__main__':
   db.create_all()
